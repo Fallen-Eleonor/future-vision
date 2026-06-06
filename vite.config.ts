6042c50a6974779a37171be3fc3831be9a47f88d
Fallen-Eleonor <wuka.bynap@gmail.com>
2026-06-06 19:04:41 +0200

    fix v2

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  
   server: {
-    host: true, 
-    port: process.env.PORT || 5173, 
-    proxy: {
-      '/api': {
-        target: process.env.VITE_API_URL || 'http://localhost:8000',
-        changeOrigin: true,
-      },
-    },
+    host: true, // Позволяет Railway получить доступ к приложению
+    port: process.env.PORT || 5173,
   },
-
   preview: {
     host: true,
     port: process.env.PORT || 5173,
