b3f2fc662b96f36391db6526a99be52c53fee913
Fallen-Eleonor <wuka.bynap@gmail.com>
2026-06-06 19:01:30 +0200

    fix port problem

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,20 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  
   server: {
-    port: 5173,
+    host: true, 
+    port: process.env.PORT || 5173, 
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: process.env.VITE_API_URL || 'http://localhost:8000',
         changeOrigin: true,
       },
     },
   },
+
+  preview: {
+    host: true,
+    port: process.env.PORT || 5173,
+  }
 })
